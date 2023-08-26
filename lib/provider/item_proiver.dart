5d35aca99e66b6d2ec29ae9c6c324b50aaceac28
christian-iuil <okoriec01@gmail.com>
2023-08-26 11:02:23 +0100

    working on the homepage list builder

--- lib/provider/item_proiver.dart
+++ lib/provider/item_proiver.dart
@@ -1,8 +1,11 @@
+import 'package:daily_item_stream/model/item.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'item_box_provder.dart';
 
-final itemProvider = FutureProvider((ref) async {
-  final box = await ref.watch(itemBoxProvider.future);
-  return box.values.toList();
-});
+final itemProvider = FutureProvider<List<Item>>(
+  (ref) async {
+    final box = await ref.watch(itemBoxProvider.future);
+    return box.values.toList();
+  },
+);
--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,11 +1,31 @@
-
+import 'package:daily_item_stream/provider/item_proiver.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends ConsumerWidget {
   const Homepage({super.key});
 
+  get data => null;
+
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: ref.watch(itemProvider).when(
+          data: (item) => ListView.builder(
+              itemCount: item.length,
+              itemBuilder: (context, index) {
+                final data = item[index];
+                return Column(
+                  children: [
+                    Text(data.title),
+                    Text(data.des),
+                  ],
+                );
+              }),
+          error: (error, stacktrace) => Text(error.toString()),
+          loading: () => const Center(
+                child: Text('Loading'),
+              )),
+    );
   }
 }
